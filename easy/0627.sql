d132c8f5a5ec167c7818c6a24d8e27963f55db4d
tinghaolai <analth355@gmail.com>
2022-04-18 22:38:16 +0800

    0627 update submit solution

--- easy/0627.sql
+++ easy/0627.sql
@@ -1,3 +1,4 @@
--- github is broken, but I thinks this is collect answer
+-- Runtime: 534 ms, faster than 5.01% of MySQL online submissions for Swap Salary.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Swap Salary.
 
 update salary set sex = if(sex='m','f','m');
